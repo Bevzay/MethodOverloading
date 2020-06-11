14de3c88ca3b64aa8366825093b113582233e6cc
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2020-06-11 17:56:58 +1200

    Method overloading

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+        calculateScore(75);
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score +" points");
+        return score * 1000;
+    }
+
+    // method overloading
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player scored " + score + " points");
+        return score * 1000;
     }
 }
